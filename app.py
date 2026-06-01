0a2fa210f941424a50ba5266aa134d97bee6dc7b
dewangga <255674162+dewanggaabdullah@users.noreply.github.com>
2026-06-01 21:05:47 +0700

    refactor1: fix error migrate file
    Signed-off-by: dewangga <255674162+dewanggaabdullah@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-import instance  # Mengimpor helper JSON milikmu (muat_data & simpan_data)
+import inventory  # Mengimpor helper JSON milikmu (muat_data & simpan_data)
 
 # Inisialisasi FastAPI
 app = FastAPI(title="Sistem Manajemen Inventaris Toko API")
@@ -7,13 +7,13 @@ app = FastAPI(title="Sistem Manajemen Inventaris Toko API")
 # 1. LIHAT DAFTAR BARANG (GET)
 @app.get("/inventaris")
 def api_lihat_daftar():
-    daftar_inventaris = instance.muat_data()
+    daftar_inventaris = inventory.muat_data()
     return daftar_inventaris  # FastAPI otomatis mengubah dictionary jadi format JSON API
 
 # 2. TAMBAH BARANG BARU (POST)
 @app.post("/inventaris")
 def api_tambah_barang(nama: str, stok: int):
-    daftar_inventaris = instance.muat_data()
+    daftar_inventaris = inventory.muat_data()
     
     # Validasi: jika barang sudah ada
     if nama in daftar_inventaris:
@@ -21,14 +21,14 @@ def api_tambah_barang(nama: str, stok: int):
         
     # Masukkan data baru ke dictionary dan simpan ke JSON
     daftar_inventaris[nama] = {"stok": stok}
-    instance.simpan_data(daftar_inventaris)
+    inventory.simpan_data(daftar_inventaris)
     
     return {"status": "sukses", "pesan": f"Berhasil menambahkan {nama} dengan stok {stok}"}
 
 # 3. ATUR ULANG STOK / OVERRIDE (PUT)
 @app.put("/inventaris/{nama}")
 def api_update_stok(nama: str, stok_baru: int):
-    daftar_inventaris = instance.muat_data()
+    daftar_inventaris = inventory.muat_data()
     
     # Validasi: Pastikan barangnya ada dulu di JSON sebelum di-update
     if nama not in daftar_inventaris:
@@ -36,14 +36,14 @@ def api_update_stok(nama: str, stok_baru: int):
         
     # Timpa stok lama dengan stok baru
     daftar_inventaris[nama]["stok"] = stok_baru
-    instance.simpan_data(daftar_inventaris)
+    inventory.simpan_data(daftar_inventaris)
     
     return {"status": "sukses", "pesan": f"Stok {nama} berhasil diubah menjadi {stok_baru}"}
 
 # 4. HAPUS BARANG (DELETE)
 @app.delete("/inventaris/{nama}")
 def api_hapus_barang(nama: str):
-    daftar_inventaris = instance.muat_data()
+    daftar_inventaris = inventory.muat_data()
     
     # Validasi: Pastikan barangnya ada sebelum dihapus
     if nama not in daftar_inventaris:
@@ -51,6 +51,6 @@ def api_hapus_barang(nama: str):
         
     # Hapus key barang dari dictionary
     del daftar_inventaris[nama]
-    instance.simpan_data(daftar_inventaris)
+    inventory.simpan_data(daftar_inventaris)
     
     return {"status": "sukses", "pesan": f"Berhasil menghapus {nama} dari inventaris"}
